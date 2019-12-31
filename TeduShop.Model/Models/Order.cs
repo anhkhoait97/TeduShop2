6fa80c1f97253c22d546615a82ea4d4e1786c92a
anhkhoait97 <anhkhoait97@gmail.com>
2019-12-31 10:37:29 +0700

    comit code data model to develop

---
+++ TeduShop.Model/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerAddress { get; set; }
+        [MaxLength(256)]
+        public string PaymentMethod { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool Status { get; set; }
+        public virtual IEnumerable<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
